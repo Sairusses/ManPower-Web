8de7a8c641f63561510e2f2eaa037f15dbfbf0e9
Sairusses <kurtarias123@gmail.com>
2026-01-14 09:28:18 +0800

    vercel friendly

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
